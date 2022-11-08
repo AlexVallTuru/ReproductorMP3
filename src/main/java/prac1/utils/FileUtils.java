473d54db3464e03de960b4ad53890139fa6672cb
martinez.cordoba.ricardo <martinez.cordoba.ricardo@alumnat.copernic.cat>
2022-11-08 16:49:45 +0100

    Así me funciona

--- src/main/java/prac1/utils/FileUtils.java
+++ src/main/java/prac1/utils/FileUtils.java
@@ -13,72 +13,79 @@ import javafx.stage.Stage;
  * @author manel
  */
 public class FileUtils {
-    
-    /***
+
+    /**
+     * *
      * Si la URL és basura de tipus Windows, elimina el nom de la unitat
-     * @return 
+     *
+     * @return
      */
-    public static String normalizeURLFormat(String url)
-    {
-        String ret = "";
-        
-        if (IdentificaOS.getOS() == IdentificaOS.TipusOS.WIN)
-            ret = url.replace("[A-Z]{1}:", "");
-        
+    public static String normalizeURLFormat(String url) {
+        String ret = url;
+
+        if (IdentificaOS.getOS() == IdentificaOS.TipusOS.WIN) {
+            //ret = url.replaceFirst("[A-Z]{1}:", "");
+            ret = url.replace("\\", "/");
+            ret = "/" + ret;
+        }
+
+        ret = "file:" + ret;
+
         return ret;
     }
-    
-    /***
-     * Retorna la ruta al MP3 de prova
-     * La ruta es torna en format URL. 
-     * 
-     * P.ex: 
-     * 
-     *  Si s'executa sobre Windows, ho retorna en el format: file:/C:/Users/abcd1234/Documents/NetBeansProjects/m03uf5prac1_22_23_enunciat/target/classes/sounds/test_sound.mp3
-     *  Si s'executa sobre Linux, ho retorna en el format: file:/home/manel/NetBeansProjects/m03uf5prac1_22_23_enunciat/target/classes/sounds/test_sound.mp3
-     * 
-     *  https://en.wikipedia.org/wiki/File_URI_scheme
-     * 
+
+    /**
+     * *
+     * Retorna la ruta al MP3 de prova La ruta es torna en format URL.
+     *
+     * P.ex:
+     *
+     * Si s'executa sobre Windows, ho retorna en el format:
+     * file:/C:/Users/abcd1234/Documents/NetBeansProjects/m03uf5prac1_22_23_enunciat/target/classes/sounds/test_sound.mp3
+     * Si s'executa sobre Linux, ho retorna en el format:
+     * file:/home/manel/NetBeansProjects/m03uf5prac1_22_23_enunciat/target/classes/sounds/test_sound.mp3
+     *
+     * https://en.wikipedia.org/wiki/File_URI_scheme
+     *
      * @param instance instància des de la qual es crida (this)
-     * @return 
+     * @return
      */
-    public static String getTestMP3(Object instance)
-    {
+    public static String getTestMP3(Object instance) {
         return instance.getClass().getClassLoader().getResource("sounds/test_sound.mp3").toString();
     }
-    
-    /***
+
+    /**
+     * *
      * Retorna una icona
-     * 
+     *
      * @param instance
      * @param nom
-     * @return 
+     * @return
      */
-    public static String getIcona(Object instance, String nom)
-    {
+    public static String getIcona(Object instance, String nom) {
         return instance.getClass().getClassLoader().getResource("icons/" + nom).toString();
     }
-    
-    /***
+
+    /**
+     * *
      * Permet seleccionar un fitxer MP3 d'una unitat de disc
-     * 
-     * @return 
+     *
+     * @return
      */
-    public static Path getMP3Fromfile()
-    {
+    public static Path getMP3Fromfile() {
         Path ret = null;
-        
+
         Stage stage1 = new Stage();
-        
+
         FileChooser filechooser1 = new FileChooser();
-        
+
         filechooser1.setTitle("Seleccionar fixter MP3");
         Path fitxerMP3 = filechooser1.showOpenDialog(stage1).toPath();
-        
-        if (fitxerMP3 != null)
+
+        if (fitxerMP3 != null) {
             ret = fitxerMP3;
-        
+        }
+
         return ret;
     }
-    
 }
